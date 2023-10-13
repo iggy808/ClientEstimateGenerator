19aab5856285ec538ce3dd3308bb88af76c640f2
Reid Trisler <reidtrisler@yahoo.com>
2023-10-13 16:21:58 -0500

    TestData

--- ClientPricingSystem/CEG.Jobs/Program.cs
+++ ClientPricingSystem/CEG.Jobs/Program.cs
@@ -6,8 +6,10 @@ class Program
     public static void Main(string[] args)
     {
         if (args.Length == 0)
+        {
             Console.WriteLine("Provide a unique job argument describing which job you would like to run. A list of CEG jobs and their respective arguments is available in the section of documentation dedicated to CEG Jobs.");
             return;
+        }
 
         switch (args.First())
         {
